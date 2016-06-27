ddbbc2768e008faeb2d55fa8297d850ba7973a05
why <triwahyuprasetyo@gmail.com>
2016-06-27 12:29:47 +0700

    update on constraint layout

--- java/com/why/sdpproject/MainActivity.java
+++ java/com/why/sdpproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.why.sdpproject;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        /*
+        setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        */
 
+/*
         editA = (EditText) findViewById(R.id.idNilaiA);
         editB = (EditText) findViewById(R.id.idNilaiB);
         editHasil = (EditText) findViewById(R.id.idHasil);
@@ -64,10 +67,10 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+*/
+        Log.i("Test", "onCreate");
 
-        Log.i("Test","onCreate");
-
-        Toast.makeText(getApplicationContext(), "The onCreate() event",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "The onCreate() event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -92,38 +95,48 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the activity is about to become visible. */
+    /**
+     * Called when the activity is about to become visible.
+     */
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(), "The onStart() event",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "The onStart() event", Toast.LENGTH_SHORT).show();
     }
 
-    /** Called when the activity has become visible. */
+    /**
+     * Called when the activity has become visible.
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(),  "The onResume() event",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "The onResume() event", Toast.LENGTH_SHORT).show();
     }
 
-    /** Called when another activity is taking focus. */
+    /**
+     * Called when another activity is taking focus.
+     */
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(),  "The onPause() event",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "The onPause() event", Toast.LENGTH_SHORT).show();
     }
 
-    /** Called when the activity is no longer visible. */
+    /**
+     * Called when the activity is no longer visible.
+     */
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(),  "The onStop() event",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "The onStop() event", Toast.LENGTH_SHORT).show();
     }
 
-    /** Called just before the activity is destroyed. */
+    /**
+     * Called just before the activity is destroyed.
+     */
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(),  "The onDestroy() event",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "The onDestroy() event", Toast.LENGTH_SHORT).show();
     }
 }
